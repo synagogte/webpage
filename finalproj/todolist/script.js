47e6c7cc40ac5bab0f2384f3df18bebd95a1fd53
synagogte <sgogte@usc.edu>
2023-06-01 13:08:28 -0700

    website copying

---
+++ finalproj/todolist/script.js
@@ -0,0 +1,63 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask()
+{
+	if (inputBox.value === '')
+		alert("you must write something");
+	else
+	{
+		let li = document.createElement("li") 
+		li.innerHTML = inputBox.value;
+		listContainer.appendChild(li);
+		let span = document.createElement("span");
+		span.innerHTML = "\u00d7";
+		li.appendChild(span);
+	}
+	inputBox.value = "";
+	saveData(); 
+}
+
+listContainer.addEventListener("click", function(e) {
+	if (e.target.tagName === "LI")
+	{
+		e.target.classList.toggle("checked"); 
+		if (e.target.classList.contains("checked"))
+		{
+			e.target.parentElement.appendChild(e.target);
+		}
+		else
+		{
+			// let firstItem = listContainer.querySelector("li");
+			var targetItems = listContainer.getElementsByClassName("checked");
+			e.target.parentElement.insertBefore(e.target, targetItems[0]);
+		}
+		saveData();
+	}
+	else if (e.target.tagName === "SPAN")
+	{
+		e.target.parentElement.remove();
+		saveData();
+	}
+}, false);
+
+function saveData()
+{
+	localStorage.setItem("data", listContainer.innerHTML); 
+}
+
+function showTask()
+{
+	listContainer.innerHTML = localStorage.getItem("data"); 
+}
+showTask();
+
+
+
+
+
+
+
+
+
+
