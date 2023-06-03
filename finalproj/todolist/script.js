d38428fd0e913c47eabc74376e7bd674602f9b4f
synagogte <sgogte@usc.edu>
2023-06-02 20:00:12 -0700

    updated

--- finalproj/todolist/script.js
+++ finalproj/todolist/script.js
@@ -9,7 +9,8 @@ function addTask()
 	{
 		let li = document.createElement("li") 
 		li.innerHTML = inputBox.value;
-		listContainer.appendChild(li);
+		var targetItem = listContainer.getElementsByClassName("checked");
+		listContainer.insertBefore(li, targetItem[0]);
 		let span = document.createElement("span");
 		span.innerHTML = "\u00d7";
 		li.appendChild(span);
